65a43ecaeb05ff78d64dc32595c5b71492a66fc3
gss121@hotmail.com <gss121@hotmail.com>
2013-03-05 18:55:32 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awiki::Application.config.secret_token = 'b5df3fac14d1948276cddc9c134d0e1c97a93f6487a8abb925b5c72c467e873a27bb08b6d1c257a5c3807a1d981060dd2007bf37275a8791f49cbb229f1490cc'
